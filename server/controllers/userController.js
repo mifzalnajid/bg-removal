9551ebbc48161dfb6c1e90668192f0ec1a49a532
mifzalnajid <mifzalnajid@gmail.com>
2025-09-29 12:48:49 +0530

    some corrections

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,59 +8,54 @@ import userModel from "../models/userModel.js"
 const clerkWebhooks = async (req, res) => {
 
     try {
-        // Create  a Svix instance with clerk webhook secret 
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        await whook.verify(JSON.stringify(req, res), {
+        // Create a Svix instance with Clerk webhook secret
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+        // Svix expects the raw body, not the parsed JSON
+        await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
-        })
+        });
 
-        const { data, type } = req.body
+        const { data, type } = req.body;
         switch (type) {
             // ----------------------------To create----------------------------------
             case "user.created": {
-
                 const userData = {
                     clerkId: data.id,
                     email: data.email_addresses[0].email_address,
-                    firstName: first_name,
+                    firstName: data.first_name,
                     lastName: data.last_name,
                     photo: data.image_url
-
-                }
-                await userModel.create(userData)
-                res.json({})
+                };
+                await userModel.create(userData);
+                res.json({ success: true });
                 break;
             }
             // --------------------For Update-----------------------------
             case "user.updated": {
                 const userData = {
                     email: data.email_addresses[0].email_address,
-                    firstName: first_name,
+                    firstName: data.first_name,
                     lastName: data.last_name,
                     photo: data.image_url
-                }
-                await userModel.findOneAndUpdate({ clerkId: data.id }, userData)
-                res.json({})
-
-                break;
-            } case "user.deleted": {
-                await userModel.findOneAndDelete({ clerkId: data.id })
-                res.json({})
-
+                };
+                await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
+                res.json({ success: true });
                 break;
             }
-
+            case "user.deleted": {
+                await userModel.findOneAndDelete({ clerkId: data.id });
+                res.json({ success: true });
+                break;
+            }
             default:
+                res.json({ success: false, message: "Unknown event type" });
                 break;
         }
-
     } catch (error) {
         console.log(error.message);
-        res.json({ success: false, message: error.message })
-
-
+        res.json({ success: false, message: error.message });
     }
 
 }
